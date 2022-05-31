6827af72e07a824b2c831dec609029a7afd45457
leon <hong-liu@sdu.edu.cn>
2022-05-31 14:16:02 +0800

    add class Lexer, some code in main to test and use Lexer

--- cbypython.py
+++ cbypython.py
@@ -1,6 +1,8 @@
 import argparse
 import sys
 
+from lexer import *
+
 
 ###############################################################################
 #                                                                             #
@@ -16,10 +18,27 @@ def main():
     parser.add_argument('input', help='C source file')
 
     args = parser.parse_args()
-
+    
+    p = args.input
+    
+    lexer = Lexer(p)
+    t = lexer.get_next_token()
     print(f"  .globl main")
     print(f"main:")
-    print(f"  mov ${args.input}, %rax")
+    print(f"  mov ${t.value}, %rax")
+    t = lexer.get_next_token()
+    while t.type != TokenType.TK_EOF:
+        if t.type == TokenType.TK_PLUS:
+            t = lexer.get_next_token()
+            print(f"  add ${t.value}, %rax")
+            t = lexer.get_next_token()
+            continue
+        elif t.type == TokenType.TK_MINUS:
+            t = lexer.get_next_token()
+            print(f"  sub ${t.value}, %rax")
+            t = lexer.get_next_token()
+            continue
+
     print(f"  ret")
 
 if __name__ == '__main__':
---
+++ lexer.py
@@ -0,0 +1,84 @@
+from enum import Enum
+
+
+class TokenType(Enum):
+    # single-character token types
+    TK_PLUS          = '+'
+    TK_MINUS         = '-'
+
+    # misc
+    TK_INTEGER_CONST = 'INTEGER_CONST'
+    TK_EOF           = 'EOF'
+
+
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+
+class Lexer:
+    def __init__(self, text):
+        # client string input, e.g. "4 + 2 * 3 - 6 / 2"
+        self.text = text
+        # self.pos is an index into self.text
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+
+    def advance(self):
+        """Advance the `pos` pointer and set the `current_char` variable."""
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None  # Indicates end of input
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def number(self):
+        """Return a (multidigit) integer or float consumed from the input."""
+
+        # Create a new token
+        token = Token(type=None, value=None)
+
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+
+        token.type = TokenType.TK_INTEGER_CONST
+        token.value = int(result)
+        return token
+
+    def get_next_token(self):
+        """Lexical analyzer (also known as scanner or tokenizer)
+
+        This method is responsible for breaking a sentence
+        apart into tokens. One token at a time.
+        """
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return self.number()
+
+            # single-character token
+            # get enum member by value, e.g.
+            # TokenType('+') --> TokenType.PLUS
+            token_type = TokenType(self.current_char)
+
+            # create a token with a single-character lexeme as its value
+            token = Token(
+                type=token_type,
+                value=token_type.value,  # e.g. '+', '-', etc
+            )
+            self.advance()
+            return token
+
+        # EOF (end-of-file) token indicates that there is no more
+        # input left for lexical analysis
+        return Token(type=TokenType.TK_EOF, value=None)
